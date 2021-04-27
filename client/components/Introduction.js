e815c9a19f354d7e11b370a452981f6b9d5184d7
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-04-27 20:52:56 +0200

    styled text cards

--- client/components/Introduction.js
+++ client/components/Introduction.js
@@ -1,26 +1,28 @@
-import React, {useState} from 'react';
-import { motion, AnimateSharedLayout, AnimatePresence } from 'framer-motion'
+import React, { useState } from 'react';
+import { motion, AnimateSharedLayout, AnimatePresence } from 'framer-motion';
 
 function Introduction() {
 	const [selectedId, setSelectedId] = useState(null);
 	return (
 		<div className='textContainer'>
-    <div className='leftCard'>
-						<motion.div className='textCard'>
-							<h1 style={{ color : '#FFFFFF'}}>Was ist der Citratzyklus</h1>
-							  <p></p>
-						</motion.div>
-						<div className='textCard'>
-							<h1 style={{ color : '#FFFFFF'}}>Anderes</h1>
-							<p></p>
-						</div>
-					</div>
-					<div className='rightCard'>
-						<div className='textCard '>
-							<h1 style={{ color : '#FFFFFF'}}>Warum ist er wichtig?</h1>
-							<p></p>
-						</div>
-					</div>
+			<div className='leftCard'>
+				<motion.div className='textCard'whileHover={{scale: 1.04, duration: 1}}>
+					<h1>
+						Was ist der Citratzyklus
+					</h1>
+					<p></p>
+				</motion.div>
+				<motion.div className='textCard' whileHover={{scale: 1.04, duration: 1}}>
+					<h1>Anderes</h1>
+					<p></p>
+				</motion.div>
+			</div>
+			<div className='rightCard'>
+				<motion.div className='textCard ' whileHover={{scale: 1.04, duration: 1}}>
+					<h1>Warum ist er wichtig?</h1>
+					<p></p>
+				</motion.div>
+			</div>
 		</div>
 	);
 }
--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+
+import { ArrowRight, FileX } from  'phosphor-react'
+
+import { animate, motion } from 'framer-motion';
 
 function Welcome() {
-  return (
-    <div className="welcomeContainer">
-      <div className="contentArea">
-      <div className="left">
-        <h1>
-          Citratzyklus
-        </h1>
-        <h3>
-          Jonas Hendel
-        </h3>
-        <Link href='https://docs.google.com/document/d/1IcOHua0btCcIxDC4L0R1cazdSkjPYELP_toNITxqBjY/edit'>
-          <a>Projektbeschreibung</a>
-        </Link>
-      </div>
-      <div className="right">
-        <img src='./biology.png'></img>
-      </div>
-      </div>
-      
-      
-    </div>
-  )
+	return (
+		<div className='welcomeContainer'>
+			<div className='contentArea'>
+				<div className='left'>
+					<h1>Citratzyklus</h1>
+					<h3>Jonas Hendel</h3>
+					<Link href='https://docs.google.com/document/d/1IcOHua0btCcIxDC4L0R1cazdSkjPYELP_toNITxqBjY/edit'>
+          <motion.div style={{ display: 'flex', alignItems: 'center', marginTop: 100}} >
+            <ArrowRight size={15}/>
+						<a style={{ marginLeft: 10 }}>Projektbeschreibung </a>
+          </motion.div>
+					</Link>
+				</div>
+				<div className='right'>
+					<motion.img
+						src='./biology.png'
+					></motion.img>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Welcome
+export default Welcome;
